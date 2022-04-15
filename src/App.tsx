710be92f1a497cbd4c0817e9d556fc14e1ab17ad
xersuy <dev.luyoes20@gmail.com>
2022-04-16 00:36:48 +0900

    backup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Header from "./components/commons/Header";
+import RotuerScrollTop from "./components/commons/RotuerScrollTop";
+import MainhomePage from "./pages/MainhomePage";
 
 const App = () => {
-  return <div className="App">aaaaaaaaaaaaaaaaaaaaaa</div>;
+  return (
+    <BrowserRouter>
+      <RotuerScrollTop />
+
+      <Routes>
+        <Route path="" element={<Navigate to="/main" />} />
+        <Route path="/main" element={<MainhomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
