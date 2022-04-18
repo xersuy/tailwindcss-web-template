f6e8290408559ebbb0ff817b68e67b133420d6b1
xersuy <dev.luyoes20@gmail.com>
2022-04-18 12:18:08 +0900

    Update package-lock.json, package.json, and App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from "react";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import Header from "./components/commons/Header";
-import RotuerScrollTop from "./components/commons/RotuerScrollTop";
-import MainhomePage from "./pages/MainhomePage";
-
+import React from 'react';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import Header from './components/commons/Header';
+import RotuerScrollTop from './components/commons/RotuerScrollTop';
+import MainhomePage from './pages/MainhomePage';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 const App = () => {
+  library.add(fas);
+
   return (
     <BrowserRouter>
       <RotuerScrollTop />
